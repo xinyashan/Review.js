2ceae1ce84d391bff3e4586679f3cb87e22e14a3
xinyashan <50379822+xinyashan@users.noreply.github.com>
2019-05-13 08:28:59 -0500

    Add files via upload
    JSHW

---
+++ JSHW/Review.js
@@ -0,0 +1,75 @@
+console.log('HelloWorld')
+
+var buddy= "Friend"; 
+var cat = "dog"; 
+console.log(buddy,cat);
+
+let buddy2 = "pal"; 
+let rocket = "monkey"; 
+const metal = "Steel"; 
+
+console.log(buddy2, rocket, metal);
+
+
+let number3= 2.4; 
+let number4 = 59.3; 
+
+let equation1 = number2 + number4 
+console.log(equation1);
+
+let number3= 2.4; 
+let number4 = 59.3; 
+
+let equation1 = number2 + number4 
+console.log(equation1);
+
+console.log("35"); 
+console.log(35); 
+
+console.log(buddy); 
+console.log("buddy"); 
+
+
+let groceryList =  ["Eggs","Milk", "Honey"]; 
+let numList = [1,2,3,4]; 
+
+console.log(groceryList); 
+console.log(numList); 
+
+for (let x = 0; x < groceryList.length; x++) {
+    console.log(x);
+    console.log(groceryList[x]); 
+}
+
+
+
+groceryList.forEach(grocery => {
+    console.log(grocery); 
+})
+
+groceryList.push("grapes"); 
+console.log(groceryList); 
+
+
+groceryList.splice(0,1); 
+console.log(groceryList); 
+
+groceryList.reverse();  
+console.log(groceryList); 
+
+console.log(typeof buddy2); 
+console.log (typeof number4); 
+
+console.log(typeof groceryList); 
+
+let groot = 35; 
+console.log('Groot is ${groot}'); 
+console.log ('Your favorite grocery is ${groceryList[1]}') 
+
+console.log(buddy2 + rocket); 
+console.log("cat"+ "dog"); 
+
+
+
+
+
